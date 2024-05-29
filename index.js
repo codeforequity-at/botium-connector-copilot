ca2640e4ede5f0bfb2d397c2f2e4d18e9ae1419e
Attila Ujj <attila.ujj@cyara.com>
2024-05-29 15:52:39 +0200

    BOT-4640 copilot connector, logo

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
 const PluginClass = require('./src/connector')
+
+const logo = fs.readFileSync(path.join(__dirname, 'logo.png')).toString('base64')
 
 module.exports = {
   PluginVersion: 1,
   PluginClass: PluginClass,
   PluginDesc: {
     name: 'Copilot',
+    avatar: logo,
     provider: 'Microsoft',
     features: {
       intentResolution: false,
